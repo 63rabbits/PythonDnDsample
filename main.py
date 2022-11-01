9f889bf6cff2260b281ce17a8c90b1ee17ea8b1f
63rabbits goodman <63rabbitsgoodman@gmail.com>
2022-11-01 20:28:27 +0900

    Add files via upload

--- main.py
+++ main.py
@@ -1,31 +1,42 @@
 # # make App
 # pip install tkinterdnd2
-# pyinstaller main.py --onefile --noconsole --icon=63rabbits.icns --collect-data tkinterdnd2
+# pyinstaller main.py --name dnd --onefile --noconsole --collect-data tkinterdnd2 --icon=icon.ico   # for windows
+# pyinstaller main.py --name dnd --onefile --noconsole --collect-data tkinterdnd2 --icon=icon.icns  # for mac
 
 from tkinter import *
 from tkinterdnd2 import *
 import utility63rabbits as util
+import os
+
+
+def put_message(message, op='APPEND'):
+    op = op.upper()
+    textbox.config(state=NORMAL)
+    if op == 'APPEND':
+        textbox.insert(END, message)
+        textbox.see(END)
+    elif op == 'CLEAR':
+        textbox.delete("1.0", END)
+    textbox.config(state=DISABLED)
 
 
 def dnd_handler(event):
     file_list = util.DND.make_file_list(event.data)
-    textbox.config(state=NORMAL)
-    textbox.delete("1.0", "end")
-    for f in file_list: textbox.insert("end", f + "\n")
-    textbox.config(state=DISABLED)
+    for f in file_list:
+        if os.path.isfile(f):     put_message(f'[file     ] {f}\n')
+        elif os.path.isdir(f):    put_message(f'[directory] {f}\n')
+        else:                     put_message(f'[???      ] {f}\n')
 
 
 # Const
-WINDOW_WIDTH = 300
+WINDOW_WIDTH = 500
 WINDOW_HEIGHT = 200
 
 # Main Window
 root = TkinterDnD.Tk()
 root.title('Drag and Drop')
 # root.geometry(f'{win_width}x{win_height}+500+100')
-# root.minsize(width=50, height=50)
 root.geometry(util.WIN.get_position(root, WINDOW_WIDTH, WINDOW_HEIGHT, 'n', 0, 50)[0])
-# root.config(bg='#cccccc')
 
 # Drag-and-Drop
 root.drop_target_register(DND_FILES)
@@ -35,8 +46,8 @@ root.dnd_bind('<<Drop>>', dnd_handler)
 frame = Frame(root)
 
 textbox = Text(frame, width=30, height=10)
-textbox.insert("end", "Drag and Drop files here.\n")
 textbox.config(state=DISABLED, wrap=NONE)
+put_message('Drag and Drop files here.\n\n')
 
 scroll_y = Scrollbar(frame, orient=VERTICAL, command=textbox.yview)
 scroll_x = Scrollbar(frame, orient=HORIZONTAL, command=textbox.xview)
